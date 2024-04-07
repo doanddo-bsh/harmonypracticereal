863b7823073c5bb2159da336befb6a340e2a0149
doanddo-bsh <dcsun57@gmail.comgit config --listgit config --global user.email dcsun57@gmail.com>
2024-04-07 23:36:07 +0900

    1문제 테스트 완료

--- lib/page/problem/problemType1.dart
+++ lib/page/problem/problemType1.dart
@@ -57,16 +57,16 @@ class _tonalityProblemType1State extends State<tonalityProblemType1> {
   @override
   Widget build(BuildContext context) {
 
-    int problemNumber = 2 ;
+    int problemNumber = 1 ;
 
     Map<int, List<List<dynamic>>> problemType1List =
     {
       // 문제번호 / 음표위치 / 정답  [-4,7,10,14]
-      1:[[Note.b.inOctave(5)
-        ,Note.c.inOctave(4)
-        ,Note.c.inOctave(4)
-        ,Note.c.inOctave(3)]
-        ,[100.0,'VI','1','2','/','VI','1','2'],],
+      1:[[Note.b.inOctave(4)
+        ,Note.f.inOctave(4)
+        ,Note.d.inOctave(4)
+        ,Note.f.inOctave(3)]
+        ,[100.0,'vii','6','4'],],
       2:[[Note.b.inOctave(5)
         ,Note.c.inOctave(4)
         ,Note.c.inOctave(4)
